8e618f5643a8424b538049df10e02c06cd20627a
Eman Khan <keman2502@gmail.com>
2022-01-25 21:49:45 +0500

    Update Main Activity

--- app/src/main/java/com/example/studentfacilitationsystem/MainActivity.java
+++ app/src/main/java/com/example/studentfacilitationsystem/MainActivity.java
@@ -2,13 +2,35 @@ package com.example.studentfacilitationsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        Button sign =  findViewById(R.id.signup);
+        sign.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, signup.class));
+            }
+
+
+        });
+
+        Button log =  findViewById(R.id.login);
+        log.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, login.class));
+            }
+
+
+        });
+    }}
